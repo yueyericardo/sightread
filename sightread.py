d0632bba8ad72489de92f53b9419c01385777dca
richard <yueyericardo@gmail.com>
2020-11-09 03:32:50 -0500

    path

--- sightread.py
+++ sightread.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 import numpy as np
+import os
 from playsound import playsound
-
-a = np.array(['1  ', '2  ', '3  ', '4  ', '5  ', '6  ', '7  '])
-b = np.array(['do ', 're ', 'mi ', 'fa ', 'sol', 'la ', 'si '])
-c = np.array(['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 
 
 def info(text):
     print('\033[32m{}\33[0m'.format(text))  # green
 
 
+a = np.array(['1  ', '2  ', '3  ', '4  ', '5  ', '6  ', '7  '])
+b = np.array(['do ', 're ', 'mi ', 'fa ', 'sol', 'la ', 'si '])
+c = np.array(['C', 'D', 'E', 'F', 'G', 'A', 'B'])
+path = os.path.dirname(os.path.realpath(__file__))
+
 info('Keep Practicing!\n')
-t = 0
 mode = input("Select Mode: \n1: Single \n2: Mix \n3: Long \n4: Custom\n")
 mode = int(mode)
 assert mode == 1 or mode == 2 or mode == 3 or mode == 4
@@ -28,8 +29,8 @@ if mode == 4:
 info("\nStart\n")
 
 while(True):
-    randints = np.random.randint(0, 7, size)
-    info(f"{' '.join(b[randints])}".format(input(f"{' '.join(a[randints])}")))
-    for i in randints:
-        playsound(f'mp3/{i+1}.m4a')
-    t += 1
+    randnotes = np.random.randint(0, 7, size)
+    info(f"{' '.join(b[randnotes])}".format(input(f"{' '.join(a[randnotes])}")))
+    for i in randnotes:
+        mp3file = os.path.join(path, f'mp3/{i+1}.m4a')
+        playsound(mp3file)
