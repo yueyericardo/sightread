a8f73a915eb3c6a173959f5cb19822ab5c5f3ab4
richard <yueyericardo@gmail.com>
2020-11-09 03:36:11 -0500

    update

--- sightread.py
+++ sightread.py
@@ -18,17 +18,17 @@ mode = input("Select Mode: \n1: Single \n2: Mix \n3: Long \n4: Custom\n")
 mode = int(mode)
 assert mode == 1 or mode == 2 or mode == 3 or mode == 4
 print()
-if mode == 1:
-    size = np.random.choice(np.arange(1, 2), 1)[0]
-if mode == 2:
-    size = np.random.choice(np.arange(1, 6), 1, p=[0.5, 0.2, 0.1, 0.1, 0.1])[0]
-if mode == 3:
-    size = np.random.choice(np.arange(3, 5), 1)[0]
 if mode == 4:
     size = int(input("Input Length: "))
 info("\nStart\n")
 
 while(True):
+    if mode == 1:
+        size = np.random.choice(np.arange(1, 2), 1)[0]
+    if mode == 2:
+        size = np.random.choice(np.arange(1, 6), 1)[0]
+    if mode == 3:
+        size = np.random.choice(np.arange(3, 5), 1)[0]
     randnotes = np.random.randint(0, 7, size)
     info(f"{' '.join(b[randnotes])}".format(input(f"{' '.join(a[randnotes])}")))
     for i in randnotes:
