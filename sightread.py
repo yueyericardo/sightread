6c91238625a3590c5d3a077095cfdd4d4c0ee3a5
richard <yueyericardo@gmail.com>
2020-11-09 03:03:23 -0500

    single

--- sightread.py
+++ sightread.py
@@ -1,23 +1,8 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.5.2
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
+#!/Users/richard/Program/anaconda3/bin/python
 import numpy as np
 import sys
 from playsound import playsound
 
-# +
 a = np.array(['1  ', '2  ', '3  ', '4  ', '5  ', '6  ', '7  '])
 b = np.array(['do ', 're ', 'mi ', 'fa ', 'sol', 'la ', 'si '])
 c = np.array(['C', 'D', 'E', 'F', 'G', 'A', 'B'])
@@ -34,7 +19,7 @@ print()
 
 while(True):
     if mode == 1:
-        size = np.random.choice(np.arange(1, 3), 1)[0]
+        size = np.random.choice(np.arange(1, 2), 1)[0]
     if mode == 2:
         size = np.random.choice(np.arange(1, 6), 1, p=[0.5, 0.2, 0.1, 0.1, 0.1])[0]
     if mode == 3:
@@ -44,6 +29,5 @@ while(True):
     for i in randints:
         playsound(f'mp3/{i+1}.m4a')
     t += 1
-# -
 
 
